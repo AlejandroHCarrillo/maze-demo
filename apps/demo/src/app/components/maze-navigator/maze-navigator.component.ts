f6eaed58a779e74fe0df1a84fc6f630328e23618
Alejandro <ahernandezdotnet@gmail.com>
2024-08-05 07:30:40 -0600

    Act 2 load maze from avaible mazes

--- apps/demo/src/app/components/maze-navigator/maze-navigator.component.ts
+++ apps/demo/src/app/components/maze-navigator/maze-navigator.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MazeService } from '../../services/maze.service';
-import { ValantDemoApiClient } from '../../api-client/api-client';
 import { fileUpload } from "../../helpers/uploadimages";
 
 @Component({
@@ -11,17 +10,15 @@ import { fileUpload } from "../../helpers/uploadimages";
 
 export class MazeNavigatorComponent implements OnInit {
   moves: string[] = [];
-  availableMazes: string[] = ["maze000", "maxe001"];
+  availableMazes: mazeItem[] = [{name:"Preloaded maze000", isSelected : true, maze:"SOXXXXXXXX\r\nOOOXXXXXXX\r\nOXOOOXOOOO\r\nXXXXOXOXXO\r\nOOOOOOOXXO\r\nOXXOXXXXXO\r\nOOOOXXXXXE" }, 
+                                {name:"Preloaded maze001", isSelected : false, maze:"S ████████\r\n   ███████\r\n █   █    \r\n████ █ ██ \r\n       ██ \r\n ██ █████ \r\n    █████E" }];
   selectedMaze: string[][] = [[]];
+  titleSelectedMaze: string = "maze001";
   
   constructor(private mazeService: MazeService) { }
-  
-  
+    
   ngOnInit(): void {
-    let vClient = ValantDemoApiClient.Client;
-
-    var mMaze = vClient.length;
-    console.log("vClient: ", vClient);
+    this.loadText('file:///C:/repos/maze-demo/apps/demo/src/assets/mazelibrary/maze001.txt');
 
     console.log("cargar movimientos: ");
     
@@ -33,8 +30,8 @@ export class MazeNavigatorComponent implements OnInit {
     .catch((e)=>{
         console.log("error: ", e);            
     });
-
-    let sMaze = this.mazeService.loadtxtFileMock("maze001");
+    
+    let sMaze = this.mazeService.loadtxtFileMock(this.titleSelectedMaze);
     // console.log(sMaze);
     
     sMaze.forEach(row => {
@@ -45,7 +42,6 @@ export class MazeNavigatorComponent implements OnInit {
   }
 
   handleFileChange(e:any){
-    // console.log("handleFileChange...");
     const file = e.target.files[0];
     if ( file ){
       this.startUploading(file);
@@ -57,13 +53,9 @@ export class MazeNavigatorComponent implements OnInit {
     
     fileUpload(file).then((fileUrl)  => {
       console.log(fileUrl);
-      // this.alumno.img = fileUrl;
-      // this.form.value.img = fileUrl;
-      // this.updateAlumno();
     });
   };
   
-
   stringToArray(inString:string){
     if (inString == undefined) return;
     return inString.split("");
@@ -72,7 +64,6 @@ export class MazeNavigatorComponent implements OnInit {
   getFile(event) {
     const input = event.target
     if ('files' in input && input.files.length > 0) {
-      // this.placeFileContent(document.getElementById('content-target'), input.files[0])
       this.placeFileContent(input.files[0])
     }
   }
@@ -81,23 +72,15 @@ export class MazeNavigatorComponent implements OnInit {
     this.readFileContent(file).then(content => {
       console.log(content);
       let rowsContent = this.stringToRows(content);      
-      // target.value = content
-      // this.selectedMaze = content;
       this.selectedMaze = this.arrRowsToMatrix(rowsContent);
+      this.titleSelectedMaze = file.name;
+      this.availableMazes.push({name:file.name, isSelected:true, maze: content + "" });
     }).catch(error => console.log(error))
   }
 
-    // placeFileContent(target, file) {
-  //   this.readFileContent(file).then(content => {
-  //     console.log(content);
-  //     let rowsContent = this.stringToRows(content);      
-  //     target.value = content
-  //     // this.selectedMaze = content;
-  //     this.selectedMaze = this.arrRowsToMatrix(rowsContent);
-  //   }).catch(error => console.log(error))
-  // }
-
   readFileContent(file) {
+    console.log(file);
+    
     const reader = new FileReader()
     return new Promise((resolve, reject) => {
       reader.onload = event => resolve(event.target.result)
@@ -121,4 +104,60 @@ export class MazeNavigatorComponent implements OnInit {
     });
     return mazetrix;
   }
+
+  selectMaze(mazeItem:mazeItem){
+    console.log("Loading Selected Maze: ", mazeItem);
+    this.availableMazes.forEach(x => x.isSelected= false);
+    mazeItem.isSelected = true;  
+    this.titleSelectedMaze = mazeItem.name;
+
+    this.selectedMaze = this.arrRowsToMatrix(this.stringToRows(mazeItem.maze));
+
+    this.readFileContent(mazeItem.name);
+  }
+
+  loadText(url) {
+    try {
+      let text = fetch(url);
+      //awaits for text.text() prop 
+      //and then sends it to readText()
+      this.readText( text ); 
+    } catch (error) {
+      console.log(error);
+            
+    }
+  }
+
+  readText(text){
+      console.log(text);
+  }
+
+  readTextFile(file) {
+    // var rawFile = new XMLHttpRequest();
+    // rawFile.open("GET", file, false);
+    // rawFile.onreadystatechange = function () {
+    //   if(rawFile.readyState === 4)  {
+    //     if(rawFile.status === 200 || rawFile.status == 0) {
+    //       var allText = rawFile.responseText;
+    //       console.log(allText);
+    //      }
+    //   }
+    // }
+    // rawFile.send(null);
+    fetch(file)
+      .then((res) => {
+        res.text()
+      })
+      .then((text) => {
+        // do something with "text"
+        console.log("text: ", text);        
+      })
+      .catch((e) => console.error(e));
+  }
+}
+
+interface mazeItem {
+  name:string, 
+  isSelected: boolean,
+  maze?: string 
 }
